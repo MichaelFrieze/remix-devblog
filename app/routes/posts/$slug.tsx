45dedbb7c15db29282e803c5f78a9ea91c8a86a1
Michael Frieze <michaelfrieze@icloud.com>
2021-11-23 18:50:58 -0500

    added a loader to access params

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,7 +1,14 @@
+import { useLoaderData } from 'remix';
+
+export let loader = async ({ params }) => {
+  return params.slug;
+};
+
 export default function PostSlug() {
+  let slug = useLoaderData();
   return (
     <div>
-      <h1>Some Post</h1>
+      <h1>Some Post: {slug}</h1>
     </div>
   );
 }
